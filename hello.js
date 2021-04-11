c060f4658ebda6108e8cd57d30e5929eb5748c16
Zobee <zohebboga@hotmail.com>
2021-04-11 15:03:01 +0000

    Functions that return stuff

--- hello.js
+++ hello.js
@@ -13,3 +13,18 @@ const sayHello  = function (name) {
 sayHello("Caliban");
 sayHello("Miranda");
 sayHello("Ferdinand");
+
+//Logs 'hello, <name> to the console, and returns undefined
+const sayHelloToConsole = function(name){
+  console.log("hello, "+name)
+}
+sayHelloToConsole('John');
+
+//Returns a string "hello, <name>", but nothing is output to the console.
+const returnSayHello = function(name){
+  return "hello, "+name
+}
+
+//Stores the return value in a variable for use elsewhere
+const greeting = returnSayHello('John');
+console.log(greeting)
